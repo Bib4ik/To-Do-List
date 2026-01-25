3841f4deec22a371eedb354c3a0161f46ce27eff
Bib4ik <vova.nerdzor@gmail.com>
2026-01-25 23:54:02 +0200

    1. Adding tasks and new lines

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Happy developing ✨')
+const list = document.querySelector('.list');
+const btn = document.querySelector('.btn');
+const li=document.querySelector('.list__item');
+
+btn.addEventListener('click', () => {
+
+    const newItem = document.createElement('li');
+    newItem.className = 'list__item';
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Введите задачу';
+
+    list.appendChild(newItem);
+    list.appendChild(input);
+});
+
+
